435afdeee9f724e2ecf20cb4f1dd3c06b5bda752
zhangjiangshan <zhangjiangshan@we.com>
2017-01-17 17:12:22 +0800

    almost finish array

--- PyCode/ArrayCode.py
+++ PyCode/ArrayCode.py
@@ -193,6 +193,230 @@ class ArrayCode:
                         if j >= k or newNums[j] != newNums[j-1]:
                             break
         return result
+
+        '''Given an array S of n integers, are there elements a, b, c and d in S such that a+b+c+d = target? 
+        Find all unique quadruplets in the array which gives the sume of target.
+        Note:Elements in quadruplets (a, b, c, d) must be in non-descending order. (ie, a<=b<=c<=d)
+        The solution must not contain duplicates quadruplets.'''
+    def fourSum(self, nums, target):
+        newNums = sorted(nums)
+        result = []
+        h = 0
+        while h < len(newNums) - 3:
+            smallTarge = target - newNums[h]      
+            tempResult = []
+            i = h + 1
+            while i < len(newNums) - 2:    
+                j = i + 1
+                k = len(newNums) - 1
+                while j < k:
+                    number = newNums[i] + newNums[j] + newNums[k]
+                    if number == smallTarge:
+                        temp = [newNums[i], newNums[j], newNums[k]]
+                        tempResult.append(temp)
+                        while True:
+                            k -= 1
+                            if j >= k or newNums[k] != newNums[k+1]:
+                                break
+                        while True:
+                            j += 1
+                            if j >= k or newNums[j] != newNums[j-1]:
+                                break
+                    elif number > smallTarge:
+                        while True:
+                            k -= 1
+                            if j >= k or newNums[k] != newNums[k+1]:
+                                break
+                    else:
+                        while True:
+                            j += 1
+                            if j >= k or newNums[j] != newNums[j-1]:
+                                break
+                while True:
+                    i += 1
+                    if i >= len(newNums) - 2 or newNums[i] != newNums[i - 1]:
+                        break
+            if len(tempResult) > 0:
+                for value in tempResult:
+                    value.insert(0, newNums[h])
+                    result.append(value) 
+            while True:
+                    h += 1
+                    if h >= len(newNums) - 3 or newNums[h] != newNums[h - 1]:
+                        break                  
+        return result
+
+    '''Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+    To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. 
+    All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.'''
+    def fourSumCount(self, A, B, C, D):
+        pass
+        
+    '''Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+    Find the minimum element.
+    You may assume no duplicate exists in the array.'''
+    def findMin(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return nums[0] if nums[0] < nums[1] else nums[1]
+        if nums[0] < nums[-1]:
+            return nums[0]
+        mid = len(nums) / 2
+        if nums[mid] > nums[0]:
+            return self.findMin(nums[mid:])
+        else :
+            return self.findMin(nums[:mid+1])
+    
+    '''Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+    Find the minimum element.
+    The array may contain duplicates.'''
+    def findMin2(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return nums[0] if nums[0] < nums[1] else nums[1]
+        start = 0
+        end = len(nums) - 1
+        while True:
+            if start >= end:
+                return nums[start]
+            elif nums[start] != nums[end]:
+                break
+            start += 1
+        if nums[start] < nums[end]:
+            return nums[start]
+        mid = start + (len(nums) - start)/ 2
+        if nums[mid] >= nums[start]:
+            return self.findMin2(nums[mid:])        
+        else :
+            return self.findMin2(nums[start:mid+1])
+        
+    '''Largest Rectangle in Histogram
+    https://leetcode.com/problems/largest-rectangle-in-histogram/'''
+    def largestRectangleArea(self, heights):
+        s = []
+        heights = heights[:]
+        heights.append(0);
+        sum = 0
+        i = 0
+        while i < len(heights):
+            if len(s) == 0 or heights[i] > heights[s[-1]]:
+                s.append(i)
+                i += 1
+            else:
+                t = s[-1]
+                del s[-1]
+                sum = max(sum, heights[t] * (i if len(s) == 0 else i - s[-1] - 1))
+        return sum
+
+    '''Given a 2D binary matrix filled with 0's and 1's, 
+    find the largest rectangle containing all ones and return its area.'''
+    def maximalRectangle(self, matrix):
+        if len(matrix) == 0:
+            return 0
+        result = 0
+        nums = map(lambda x:int(x) ,list(matrix[0]))
+        for i in range(0, len(matrix)):
+            if i != 0:
+                for idx in range(0, len(nums)):
+                    string = matrix[i]
+                    val = int(string[idx])
+                    nums[idx] = val if val == 0 else nums[idx] + val
+            tempMax = self.largestRectangleArea(nums)
+            result = max(result, tempMax)
+        return result
+    
+    '''Determine whether an integer is a palindrome. Do this without extra space.'''
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        string = str(x)
+        for i in range(0, len(string) / 2):
+            if string[i] != string[-(i+1)]:
+                return False
+        return True
+    
+    '''Write an efficient algorithm that searches for a value in an m x n matrix. 
+    This matrix has the following properties:
+    Integers in each row are sorted from left to right.
+    The first integer of each row is greater than the last integer of the previous row.'''
+    def searchMatrix(self, matrix, target):
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+        if n == 0:
+            return False
+        array = map(lambda x:x[0], matrix)
+        start = 0
+        end = m - 1
+        while end - start > 1:
+            mid = (start + end) / 2
+            if target > array[mid]:
+                start = mid
+            elif target < array[mid]:
+                end = mid
+            else:
+                return True
+        if target < array[end]:
+            i = start
+        elif target == array[end]:
+            return True
+        elif target > matrix[m-1][n-1]:
+            return False
+        else:
+            i = end
+        from bisect import bisect_left    
+        index = bisect_left(matrix[i], target) 
+        if index < len(matrix[i]) and matrix[i][index] == target:
+            return True
+        else:
+            return False
+    
+    '''Write an efficient algorithm that searches for a value in an m x n matrix. 
+    This matrix has the following properties:
+    Integers in each row are sorted in ascending from left to right.
+    Integers in each column are sorted in ascending from top to bottom.'''
+    def searchMatrix2(self, matrix, target):
+       
+        row = len(matrix)
+        if row == 0:
+            return False
+        col = len(matrix[0])
+        if col == 0:
+            return False
+        i = col - 1
+        j = 0
+        while j < row and i >= 0:
+            value = matrix[j][i]
+            if target > value:
+                j += 1
+            elif target < value:
+                i -= 1
+            else: 
+                return True
+        return False   
+       
+
+            
+                
+            
+        
+            
+            
+
+        
+                        
+                    
+                
+        
+        
+        
+    
+
         
 
             
--- test.py
+++ test.py
@@ -61,15 +61,64 @@ class ArrayTestCase(unittest.TestCase):
         funcName = sys._getframe().f_code.co_name
         print funcName
         result = self.code.threeSum([-1, 0, 1, 2, -1, -4]) 
-        print result
         assert len(result) == 2 and [-1, 0, 1] in result and [-1, -1, 2] in result, 'wrong ' + funcName 
 
     def test_threeSumClosest(self):
         funcName = sys._getframe().f_code.co_name
         print funcName
         result = self.code.threeSumClosest([1,2,4,8,16,32,64,128], 82) 
-        print result
         assert result == 82, 'wrong ' + funcName
+
+    def test_fourSum(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.fourSum([0,0,0,0], 0) == [[0,0,0,0]], 'wrong ' + funcName
+        assert self.code.fourSum([1,0,-1,0,-2,2], 0) == [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]], 'wrong ' + funcName
+    
+    def test_findMin(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.findMin([3,1,2]) == 1, 'wrong ' + funcName
+        assert self.code.findMin([4,5,6,7,0,1,2]) == 0, 'wrong ' + funcName
+    
+    def test_findMin2(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.findMin2([1,1,3,1]) == 1, 'wrong ' + funcName
+    
+    def test_largestRectangleArea(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.largestRectangleArea([3,6,5,7,4,8,1]) == 20, 'wrong ' + funcName
+        assert self.code.largestRectangleArea([2,1,5,6,2,3]) == 10, 'wrong ' + funcName
+        assert self.code.largestRectangleArea([0,0,0,0,0,0,0,0,2147483647]) == 2147483647, 'wrong ' + funcName
+
+    def test_maximalRectangle(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.maximalRectangle(["01","10"]) == 1, 'wrong ' + funcName
+        assert self.code.maximalRectangle(["0000","1111","1110","0100"]) == 6, 'wrong ' + funcName
+    
+    def test_isPalindrome(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.isPalindrome(121) == True, 'wrong ' + funcName
+    
+    def test_searchMatrix(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.searchMatrix([[1],[3]], 3) == True, 'wrong ' + funcName
+        assert self.code.searchMatrix([[1,3,5]], 0) == False, 'wrong ' + funcName
+        assert self.code.searchMatrix([[1],[3]], 2) == False, 'wrong ' + funcName
+        assert self.code.searchMatrix([[1]], 0) == False, 'wrong ' + funcName
+    
+    def test_searchMatrix2(self):
+        funcName = sys._getframe().f_code.co_name
+        print funcName
+        assert self.code.searchMatrix2([[1,4],[2,5]],2) == True, 'wrong ' + funcName 
+
+        assert self.code.searchMatrix2([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]], 15) == True, 'wrong ' + funcName 
+        assert self.code.searchMatrix2([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5) == True, 'wrong ' + funcName  
 
     def tearDown(self):
         pass
